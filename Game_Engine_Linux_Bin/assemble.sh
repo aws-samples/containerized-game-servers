7cbe330136338ae2d24525d65d8bbe32f194cee1
Yahav Biran <birayaha@amazon.com>
2019-09-18 16:10:40 -0700

    revert changes

--- Game_Engine_Linux_Bin/assemble.sh
+++ Game_Engine_Linux_Bin/assemble.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-cat BinLinux64.Dedicated.tar.gz.x* > ../BinLinux64.Dedicated.tar.gz
+cat BinLinux64.Dedicated.tar.gz.x* > BinLinux64.Dedicated.tar.gz
+tar xvzf BinLinux64.Dedicated.tar.gz
+rm BinLinux64.Dedicated.tar.gz*

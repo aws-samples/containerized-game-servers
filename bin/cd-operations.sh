078c5be1fae148803c089d06abdfe21b0a750bf8
Yahav Biran <birayaha@amazon.com>
2019-10-07 15:03:26 -0700

    Update cd-operations.sh

--- bin/cd-operations.sh
+++ bin/cd-operations.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-configmaplist=`find ../workshop/eks/specs -name "*-config.yaml"`
+configmaplist=`find ./workshop/eks/specs -name "*-config.yaml"`
 for configmap in $configmaplist
 do
   kubectl apply -f $configmap
 done
 
-speclist=`find ../workshop/eks/specs -name "*-deploy.yaml"`
+speclist=`find ./workshop/eks/specs -name "*-deploy.yaml"`
 for spec in $speclist
 do 
   kubectl apply -f $spec

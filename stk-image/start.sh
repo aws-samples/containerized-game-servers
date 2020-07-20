304efc31f93de1a669ae1242ba3ecc8640149a53
Biran <birayaha@f8ffc21f4dbb.ant.amazon.com>
2020-07-19 17:40:55 -0700

    add sigterm handler for deregister call

--- stk-image/start.sh
+++ stk-image/start.sh
@@ -1,2 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+trap sighandler TERM;
+
+sighandler() {
+  echo -en "\n## Caught SIGTERM\n";
+  echo -en "\n## deregister game-server comes here\n";
+  exit $?
+}
+
+
+port=`kubectl get svc $POD_NAME -n $NAMESPACE -o json|jq '.spec.ports[].nodePort'`
+hostname=`curl 169.254.169.254/latest/meta-data/public-hostname`
+game_server_endpoint=$hostname:$port
+echo "register $game_server_endpoint"
 ./cmake_build/bin/supertuxkart --server-config=/home/supertuxkart/stk-code/server_config.xml

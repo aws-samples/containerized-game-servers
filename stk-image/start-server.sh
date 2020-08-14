0f84f7fd8244d215e11d8e7a00d5d72c302bae60
Biran <birayaha@f8ffc21f4dbb.ant.amazon.com>
2020-08-13 21:28:27 -0700

    add fleetiq and agones health and ready indications

--- stk-image/start-server.sh
+++ stk-image/start-server.sh
@@ -8,5 +8,6 @@ sighandler () {
   aws gamelift deregister-game-server --game-server-group-name $GAME_SERVER_GROUP_NAME --game-server-id $game_server_id 
   exit $?
 }
-
+sleep 10
+curl -d "{}" -H "Content-Type: application/json" -X POST http://localhost:${AGONES_SDK_HTTP_PORT}/ready
 ./cmake_build/bin/supertuxkart --server-config=/home/supertuxkart/stk-code/server_config.xml

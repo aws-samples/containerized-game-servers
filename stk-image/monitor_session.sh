ce366aaa1098ba1baa62f9f4d707fde70e0a7ce8
Biran <birayaha@f8ffc21f4dbb.ant.amazon.com>
2020-08-18 09:44:43 -0700

    remove fleetiq calls

--- stk-image/monitor_session.sh
+++ stk-image/monitor_session.sh
@@ -25,14 +25,14 @@ do
       echo num_of_no_session_events=$num_of_no_session_events
       if [ "$num_of_no_session_events" -ge "$NUM_IDLE_SESSION" ]; then 
         echo "no acvite sessions in the last $FREQ_CHECK_SESSION*$num_of_no_session_events seconds, going to terminate the game server"
-        aws gamelift deregister-game-server --game-server-group-name $game_server_name --game-server-id $game_server_id
-        kubectl delete pod $POD_NAME -n $NAMESPACE
+        echo aws gamelift deregister-game-server --game-server-group-name $game_server_name --game-server-id $game_server_id
+        echo kubectl delete pod $POD_NAME -n $NAMESPACE
       fi
     fi
     echo $current_time $last_session_packet_time
-    aws gamelift update-game-server --game-server-group-name $game_server_name --game-server-id $game_server_id --utilization-status "UTILIZED"
+    echo aws gamelift update-game-server --game-server-group-name $game_server_name --game-server-id $game_server_id --utilization-status "UTILIZED"
   fi
   curl -d "{}" -H "Content-Type: application/json" -X POST http://localhost:${AGONES_SDK_HTTP_PORT}/health
-  aws gamelift update-game-server --game-server-group-name $game_server_name --game-server-id $game_server_id --health-check "HEALTHY"
+  echo aws gamelift update-game-server --game-server-group-name $game_server_name --game-server-id $game_server_id --health-check "HEALTHY"
   sleep ${FREQ_CHECK_SESSION}
 done
--- stk-image/start-server.sh
+++ stk-image/start-server.sh
@@ -5,7 +5,7 @@ trap sighandler TERM QUIT EXIT KILL
 sighandler () {
   echo -en "\n## Caught SIGTERM\n";
   echo -en "\n## deregister game-server $game_server_endpoint $game_server_id \n";
-  aws gamelift deregister-game-server --game-server-group-name $GAME_SERVER_GROUP_NAME --game-server-id $game_server_id 
+  echo aws gamelift deregister-game-server --game-server-group-name $GAME_SERVER_GROUP_NAME --game-server-id $game_server_id 
   exit $?
 }
 sleep 10

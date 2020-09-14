0bdd26274a2911e9ef7b1c6e201cbe2cea8a540c
Biran <birayaha@f8ffc21f4dbb.ant.amazon.com>
2020-09-14 13:52:17 -0700

    agones-gs-liveness-probe-fix

--- stk-image/start.sh
+++ stk-image/start.sh
@@ -9,8 +9,8 @@ if [ "${NUM_IDLE_SESSION}" == "" ]; then
   echo '[ERROR] Env variable NUM_IDLE_SESSION has no value set' 1>&2
   exit 1
 fi
-#/populate_game_server_attr.sh 1>&2 
-#tcpdump -l -n udp port 8080 > /capture_file &
+/populate_game_server_attr.sh 1>&2 
+tcpdump -l -n udp port 8080 > /capture_file &
 tail -F ~/.config/supertuxkart/config-0.10/server_config.log | egrep 'GameProtocol.*Controller action|STKHost.*has just connected. There are now' > /var/log/stk.log &
-#/monitor_session.sh 1>&2 &
+/monitor_session.sh 1>&2 &
 /home/supertuxkart/stk-code/start-server.sh 1>&2

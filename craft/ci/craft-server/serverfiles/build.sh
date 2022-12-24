0b14a485bdaae2eb4d312e43ea0cc5cf87817ea9
Biran <birayaha@3c063041fedb.ant.amazon.com>
2022-12-23 21:58:43 -0800

    /craft/agones

--- craft/ci/craft-server/serverfiles/build.sh
+++ craft/ci/craft-server/serverfiles/build.sh
@@ -7,5 +7,5 @@ repo_name='.dkr.ecr.'$region'.amazonaws.com/'$repo':aarch64py3agones'
 repo_url=$account$repo_name
 #docker rmi `docker images| grep $repo | awk '{print $3}'` --force
 aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $repo_url
-docker build -t $repo_url --build-arg GITHUB_CRAFT_VERSION=$GITHUB_CRAFT_VERSION -f ./Dockerfile .
+docker build -t $repo_url --build-arg GITHUB_CRAFT_BRANCH=$GITHUB_CRAFT_BRANCH -f ./Dockerfile .
 docker push $repo_url

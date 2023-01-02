069987896e5bdc5dbe67e95ef501dd1000b42640
Biran <birayaha@3c063041fedb.ant.amazon.com>
2023-01-02 15:53:21 -0800

    cleanup

--- craft/ci/craft-server/serverfiles/create-ecr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-  
-echo "Creating docker image repositories"
-aws cloudformation create-stack --stack-name ecr-craft-repo --template-body file://./ecr-craft-repo.json
--- craft/ci/craft-server/serverfiles/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PGUSER=`cat $SECRET_FILE | jq -r '.username'`
-export PGDATABASE=`cat $SECRET_FILE | jq -r '.username'`
-export PGPASSWORD=`cat $SECRET_FILE | jq -r '.password'`
-export PGHOST=`cat $SECRET_FILE | jq -r '.host'`
-export PGPORT=`cat $SECRET_FILE | jq -r '.port'`
-cd craft
-./monitor_active_game_sessions.sh&
-python server.py

d76b903ca8de10e5a7c9efd1ce0762a8343ada59
Biran <birayaha@3c063041fedb.ant.amazon.com>
2023-01-05 20:03:58 -0800

    fleet scale

--- craft/server/ci/craft-server/serverfiles/monitor_active_game_sessions.sh
+++ craft/server/ci/craft-server/serverfiles/monitor_active_game_sessions.sh
@@ -4,7 +4,7 @@ cooldown_after_scale_up=$SCALE_UP_COOLDOWN
 is_cooled_down=0
 while true
 do
-  current_num_of_game_servers=$(kubectl get deploy $DEPLOY_NAME | grep -v NAME| awk '{print $3}')
+  current_num_of_game_servers=$(kubectl get fleet $DEPLOY_NAME | grep -v NAME | awk '{print $4}')
   num_of_active_sessions=$(netstat -anp | grep $(/sbin/ip addr| grep inet | grep -v inet6| grep -v 127.0.0.1| awk '{print $2}'| awk -F\/ '{print $1}') | grep 4080 | grep ESTABLISHED| wc -l)
   echo `date` num_of_active_sessions=$num_of_active_sessions num_of_game_servers=$current_num_of_game_servers
   if (( $num_of_active_sessions >= $max_sessions_per_game_server ))
@@ -13,7 +13,7 @@ do
     then
       echo "game server is at capacity; max_sessions_per_game_server=$max_sessions_per_game_server going to add more servers"
       new_num_of_game_servers=$(echo $(( $current_num_of_game_servers + 1 )))
-      kubectl scale deploy $DEPLOY_NAME --replicas=$new_num_of_game_servers
+      kubectl scale fleet $DEPLOY_NAME --replicas=$new_num_of_game_servers
       echo cooling down for $cooldown_after_scale_up sec
       sleep $cooldown_after_scale_up
       is_cooled_down=1

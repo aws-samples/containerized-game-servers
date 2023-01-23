1a1a662525cdc865f374c096e83b00d9b5461db7
Biran <birayaha@3c063041fedb.ant.amazon.com>
2023-01-23 11:08:10 -0800

    multi-arch arm64 and amd64 builds

--- craft/server/base-image-arm64v8-python3/src/create-ecr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-  
-echo "Creating docker image repositories"
-cat ecr-baseimage-repos.json.template | envsubst > ecr-baseimage-repos.json
-aws cloudformation create-stack --stack-name ecr-baseimage-repos --template-body file://./ecr-baseimage-repos.json
